d8e41ab6567916e3fb59dd2e889936e6864b43a9
Kai Lauterbach <klaute@gmx.de>
2022-05-17 12:54:47 +0200

    First try to get only the windspeed

--- index.js
+++ index.js
@@ -1,79 +1,17 @@
-var Service, Characteristic;
-var request = require('request');
+// homebridge-ws/index.js
+// Copyright © 2018-2022 Erik Baauw. All rights reserved.
+//
+// Homebridge plugin for virtual weather station.
+//
+// Modified by: Kai Lauterbach (05/2022)
+//
+// Removed all except of Windspeed
 
-const DEF_MIN_SPEED = 0,
-      DEF_MAX_SPEED = 800,
-      DEF_TIMEOUT = 5000;
+'use strict'
+
+const WsPlatform = require('./lib/WsPlatform')
+const packageJson = require('./package.json')
 
 module.exports = function (homebridge) {
-   Service = homebridge.hap.Service;
-   Characteristic = homebridge.hap.Characteristic;
-   homebridge.registerAccessory("homebridge-http-windspeed", "HttpWindspeed", HttpWindspeed);
+  WsPlatform.loadPlatform(homebridge, packageJson, 'WS', WsPlatform)
 }
-
-
-function HttpWindspeed(log, config) {
-   this.log = log;
-
-   // url info
-   this.url = config["url"];
-   this.http_method = config["http_method"] || "GET";
-   this.name = config["name"];
-   this.manufacturer = config["manufacturer"] || "@klaute";
-   this.model = config["model"] || "nodeMCU multi sensor DIY";
-   this.serial = config["serial"] || "20220516";
-   this.timeout = config["timeout"] || DEF_TIMEOUT;
-   this.minWindSpeed = config["min_lux"] || DEF_MIN_SPEED;
-   this.maxWindSpeed = config["max_lux"] || DEF_MAX_SPEED;
-}
-
-HttpWindspeed.prototype = {
-
-   getState: function (callback) {
-      var ops = {
-         uri:    this.url,
-         method: this.http_method,
-         timeout: this.timeout
-      };
-
-      //Parse the request sent via http
-      this.log('Requesting windspeed on "' + ops.uri + '", method ' + ops.method);
-      request(ops, (error, res, body) => {
-         var value = null;
-         if (error) {
-            this.log('HTTP bad response (' + ops.uri + '): ' + error.message);
-         } else {
-            try {
-               value = JSON.parse(body).windspeed;
-               if (value < this.minLux || value > this.maxLux || isNaN(value)) {
-                  throw "Invalid value received";
-               }
-               this.log('HTTP successful response: ' + body);
-            } catch (parseErr) {
-               this.log('Error processing received information: ' + parseErr.message);
-               error = parseErr;
-            }
-         }
-         callback(error, value);
-      });
-   },
-
-   getServices: function () {
-      this.informationService = new Service.AccessoryInformation();
-      this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-      .setCharacteristic(Characteristic.Model, this.model)
-      .setCharacteristic(Characteristic.SerialNumber, this.serial);
-
-      this.windspeedService = new Service.eve.WindSensor(this.name);
-      this.windspeedService
-         .getCharacteristic(Characteristic.eve.WindSpeed)
-         .on('get', this.getState.bind(this))
-         .setProps({
-             minValue: this.minWindSpeed,
-             maxValue: this.maxWindSpeedx
-         });
-      return [this.informationService, this.windspeedService];
-   }
-};
-
